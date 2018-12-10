88446e9b1e20250815c3bdb2f214f9c18492e2cd
Cássio de Freitas e Silva <cassio@codebit.com.br>
2018-12-10 17:32:11 -0200

    Added a simple example and some WAI attributes to modal.

--- codebit-modal.js
+++ codebit-modal.js
@@ -27,6 +27,8 @@ function createCbModalSkeleton() {
     return backdrop;
 }
 
+var showingCbModals = [];
+
 function CbModal(element) {
     this.backdrop = element || createCbModalSkeleton();
     this.animationTime = 200; // same as CSS transition (.show)
@@ -34,10 +36,33 @@ function CbModal(element) {
     this.onShown = null;
     this.onHidden = null;
 
+    var self = this;
+    this.backdrop.modal = this;
+
     this.modal = this.backdrop.getElementsByClassName("cb-modal")[0];
     this.modalHead = this.backdrop.getElementsByClassName("cb-modal-head")[0];
     this.modalBody = this.backdrop.getElementsByClassName("cb-modal-body")[0];
     this.modalFoot = this.backdrop.getElementsByClassName("cb-modal-foot")[0];
+
+    this.guid = function () {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    };
+
+    this.modalHead.id = this.guid();
+    this.modalBody.id = this.guid();
+
+    this.modal.setAttribute("role", "modal");
+    this.modal.setAttribute("aria-modal", "true");
+    this.modal.setAttribute("aria-labelledby", this.modalHead.id);
+    this.modal.setAttribute("aria-describedby", this.modalBody.id);
+
+    this.modalBody.tabIndex = -1;
 
     this.blurBackdrop = function (blur) {
         var sameLevelElements = this.backdrop.parentElement.children;
@@ -51,16 +76,26 @@ function CbModal(element) {
                 e.classList.remove("cb-modal-blur");
             }
         }
-    }
+    };
 
     this.removeFromParent = function () {
         var parent = this.backdrop.parentElement;
         if (parent != null) parent.removeChild(this.backdrop);
-    }
+    };
 
-    this.show = function (zIndex) {
+    this.show = function (childOf) {
         var cbModal = this;
         this.backdrop.style.display = "block";
+
+        var zIndex = 5;
+        if (childOf != null) {
+            if (childOf.backdrop == null || childOf.backdrop.style == null || childOf.backdrop.style.zIndex == null) {
+                console.warn('The object passed as "childOf" is not a valid CbModal.');
+            } else {
+                this.childOf = childOf;
+                zIndex = parseInt(childOf.backdrop.style.zIndex) + 1;
+            }
+        }
         this.backdrop.style.zIndex = zIndex || '5';
 
         // insert element on root
@@ -74,12 +109,17 @@ function CbModal(element) {
             setTimeout(function () {
                 if (cbModal.onShown != null) cbModal.onShown();
             }, this.animationTime);
+            showingCbModals.push(self);
         }, 50);
         cbModal.blurBackdrop(true);
 
         if (document.activeElement != null) {
             document.activeElement.blur();
         }
+    };
+
+    this.setZIndex = function (zIndex) {
+        this.backdrop.style.zIndex = zIndex;
     };
 
     this.hide = function () {
@@ -90,6 +130,17 @@ function CbModal(element) {
             cbModal.blurBackdrop(false);
             if (cbModal.disattachWhenHidden) cbModal.removeFromParent();
             if (cbModal.onHidden != null) cbModal.onHidden();
+            showingCbModals.splice(showingCbModals.indexOf(self));
         }, this.animationTime);
-    }
+    };
 }
+
+document.addEventListener('keydown', function (e) {
+    if (e.key == "Escape") {
+        if (showingCbModals.length != 0) {
+            var modal = showingCbModals[showingCbModals.length - 1];
+            modal.hide();
+            e.stopPropagation();
+        }
+    }
+});
